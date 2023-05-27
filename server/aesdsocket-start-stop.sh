e06e1e5cbb1baa92d93b30bdbec407bf8170ae6a
nightmare9527 <kuohowhuang1@gmail.com>
2023-05-27 17:02:00 +0800

    tryyy

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -7,17 +7,13 @@ case "$1" in
         echo "AESD Socket Server Started"
         start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
         ;;
-  
-  
   stop)
         echo "AESD Socket Server Stopped"
         start-stop-daemon -K -n aesdsocket --signal TERM
-        ;;
-        
+        ;;       
         *)
           echo "COMMAND USAGE: $0 {start|stop}"
-          
-  exit 1
+          exit 1
   
  esac
  
